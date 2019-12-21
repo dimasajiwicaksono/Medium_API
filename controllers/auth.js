675967342021c3e7a32a3f621a73e0b6a850b5e0
dimasajiwicaksono <dimasajiwicaksono@student.uns.ac.idclear>
2019-12-21 17:59:04 +0700

    revisi

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,18 @@
 
-const jwt = require ('jsonwebtoken')
+const jwt = require ('jsonwebtoken');
 
-const models = require('../models')
-const User = models.users
+const models = require('../models');
+const User = models.users;
 
 exports.login = (req, res) => {
     // check email and pass when macth with db
 
-    const email = req.body.email
-    const password = req.body.password // use encrypt in realcase
+    const email = req.body.email ;
+    const password = req.body.password ; // use encrypt in realcase
 
     User.findOne({where: {email, password}}).then(users=>{
         if(User){
-            const token = jwt.sign({ userId: users.id }, 'my-secret-key')
+            const token = jwt.sign({ userId: users.id }, 'my-secret-key');
             res.send({
                 users,
                 token
@@ -21,8 +21,8 @@ exports.login = (req, res) => {
             res.send({
                 error: true,
                 message: "Wrong Email or Password!"
-            })
+            }) ;
         }
-    }).catch (err => res.send(err))
-}
+    }).catch (err => res.send(err));
+};
 
