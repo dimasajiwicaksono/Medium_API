79b1a5086836c6c4f24759641380dfd0f8a66424
dimasajiwicaksono <dimasajiwicaksono@student.uns.ac.idclear>
2019-12-21 18:58:02 +0700

    typo PORT

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.POST || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors()) ;
 app.use(bodyParser.json()) ;
